4338afc65612f277a8d51069eea19967ed938399
Cjacobs20 <christianejacobs2020@gmail.com>
2026-06-09 21:28:02 -0600

    removal of blasphamus incantations of the replicated soul.

--- js/cursor.js
+++ js/cursor.js
@@ -1,15 +1,10 @@
 (function () {
   const cursor = document.getElementById('cursor');
-  const ring = document.getElementById('cursorRing');
-  let mx = -100, my = -100, rx = -100, ry = -100;
+  let mx = -100, my = -100;
   document.addEventListener('mousemove', function (e) { mx = e.clientX; my = e.clientY; });
   (function animCursor() {
     cursor.style.left = mx + 'px';
     cursor.style.top = my + 'px';
-    rx += (mx - rx) * 0.12;
-    ry += (my - ry) * 0.12;
-    ring.style.left = rx + 'px';
-    ring.style.top = ry + 'px';
     requestAnimationFrame(animCursor);
   })();
 })();
